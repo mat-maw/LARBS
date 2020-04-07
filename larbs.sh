b787efcbe6680e0392a959b1312c8971d3710175
MaW <maw@aaathats3as.com>
2020-04-07 18:40:42 +0200

    configs and progs from own repo

--- larbs.sh
+++ larbs.sh
@@ -14,8 +14,8 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
 	*) printf "Invalid option: -%s\\n" "$OPTARG" && exit ;;
 esac done
 
-[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/progs.csv"
+[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/mat-maw/voidrice.git"
+[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/mat-maw/LARBS/maw/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
 [ -z "$repobranch" ] && repobranch="master"
 
